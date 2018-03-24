5482c5ae4948ce3b0ed827f29a6d9e38fecc8182
lagmanzaza <lagmanmc@gmail.com>
2018-03-24 16:43:20 +0700

    [Mongoose , Service] chat group service

---
+++ src/models/chat.model.js
@@ -0,0 +1,27 @@
+// chat-model.js - A mongoose model
+// 
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+module.exports = function (app) {
+  const mongooseClient = app.get('mongooseClient');
+  const {
+    Schema
+  } = mongooseClient;
+  const chat = new Schema({
+    usersInChatRoom: [{
+      name: {
+        type: String
+      },
+      userRole: {
+        type: String
+      },
+      text: {
+        type: String
+      }
+    }]
+  }, {
+    timestamps: true
+  });
+
+  return mongooseClient.model('chat', chat);
+};
